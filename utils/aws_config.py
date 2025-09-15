4a1834dd61b045354b00c39946282466c8eb4e9b
mehulgoel-25 <mehul.g@odndigital.com>
2025-09-15 14:31:11 +0530

    updated_code

--- utils/aws_config.py
+++ utils/aws_config.py
@@ -80,8 +80,7 @@ def download_all_images_from_pose_bucket(download_dir=TRAINING_DIR / "training_i
     return downloaded_data
 
 
-# if __name__ == "__main__":
-#     metadata = download_all_images_from_pose_bucket()
-#     print("Total logged:", len(metadata))
+metadata = download_all_images_from_pose_bucket()
+print("Total logged:", len(metadata))
 
 
