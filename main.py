b6ac3ef33ac813df62826ec2af205c900ad232d7
mehulgoel-25 <mehul.g@odndigital.com>
2025-09-17 10:25:12 +0530

    final_updated_code

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import argparse
 from utils.train_model import train
+from utils.merge_details import merge_detail_poses
+from utils.aws_config import download_all_images_from_pose_bucket
 
 def main():
-    parser = argparse.ArgumentParser(description="Train ResNet50 on Custom Dataset")
+    
+    metadata = download_all_images_from_pose_bucket()
+    print("Total downloaded:", len(metadata))
+    merge_detail_poses()
 
+    parser = argparse.ArgumentParser(description="Train ResNet50 on Custom Dataset")
     parser.add_argument(
         "--epochs",
         type=int,
@@ -18,6 +24,7 @@ def main():
     )
 
     args = parser.parse_args()
+
     train(epochs=args.epochs, batch_size=args.batch_size)
 
 if __name__ == "__main__":
--- utils/merge_details.py
+++ utils/merge_details.py
@@ -1,16 +1,18 @@
 import json
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-json_path = BASE_DIR / "Configs" / "training.json"
+def merge_detail_poses():
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    json_path = BASE_DIR / "Configs" / "training.json"
 
-if json_path.exists():
-    with open(json_path, "r") as f:
-        data = json.load(f)
+    if json_path.exists():
+        with open(json_path, "r") as f:
+            data = json.load(f)
 
-    for img_name, info in data.items():
-        if "pose" in info and info["pose"] in ["detail_front", "detail_top"]:
-            info["pose"] = "detail"
+        for img_name, info in data.items():
+            if "pose" in info and info["pose"] in ["detail_front", "detail_top"]:
+                info["pose"] = "detail"
 
-    with open(json_path, "w") as f:
-        json.dump(data, f, indent=4)
+        with open(json_path, "w") as f:
+            json.dump(data, f, indent=4)
+
